43989d387cfd6a5f635b251e6ab3aa717fe6e55e
Francisco J. Ruiz Villar <franciscoruizvillar@gmail.com>
2026-08-31 21:40:01 +0000

    Operational crawler added

--- p1serial_docker/p1serial.py
+++ p1serial_docker/p1serial.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import logging
+import os
 import re
 import time
-
 from datetime import datetime
 from enum import Enum
 from logging.handlers import TimedRotatingFileHandler
@@ -10,8 +10,12 @@ from logging.handlers import TimedRotatingFileHandler
 import crcmod
 import pytz
 import serial
-
+import influxdb
 from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBServerError
+
+#from influxdb_client import InfluxDBClient
+#from flightsql import FlightSQLClient
 
 # Create the InfluxDB client object
 IDB_MEASUREMENT = "p1data"
@@ -26,22 +30,22 @@ DRM4_LINE_SEP = '\r\n'
 TZ_DRM4 = pytz.timezone("Europe/Amsterdam")
 TZ_INFLUX = pytz.utc
 
+INTERVAL = 0
+LOOP = True
 
-drm4_crc = crcmod.mkCrcFun(0x18005, rev=False)  # FIXME: Find the way in which
+drm4_crc = crcmod.mkCrcFun(0x18005, rev=False)
 
 logging.basicConfig(
-    handlers=[TimedRotatingFileHandler('/home/pi/p1/log/main.log', when='D', backupCount=7)],
-    format="[%(asctime)s] [%(levelname)s] %(message)s",
-    datefmt='%Y-%m-%dT%H:%M:%S')
-
-Logger = logging.getLogger()
+        handlers=[TimedRotatingFileHandler('crawl_data.log', when='D', backupCount=7)],
+        format="[%(asctime)s] [%(levelname)s] %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
 
 
 class Drm4(Enum):
     """ https://www.netbeheernederland.nl/_upload/Files/Slimme_meter_15_32ffe3cc38.pdf """
     VERSION = (1, 3, 0, 2, 8)  # unused
 
-    TIMESTAMP_ELECTRICITY = (0, 0, 1, 0, 0)
+    TIMESTAMP_ELECTR = (0, 0, 1, 0, 0)
 
     EQ_ID = (0, 0, 96, 1, 1)  # unused
 
@@ -52,11 +56,14 @@ class Drm4(Enum):
     CURRENT_A = (1, 0, 31, 7, 0)
     GAS_T_VOLUME_M3 = (0, 1, 24, 2, 1)
 
+    # Solar panels!
+    READ_RET_T1_KWH = (1, 0, 2, 8, 1)
+    READ_RET_T2_KWH = (1, 0, 2, 8, 2)
+
     # These we receive but we don't actually use!
-    UNUSED_01 = (1, 0, 2, 8, 1)
-    UNUSED_02 = (1, 0, 2, 8, 2)
-    UNUSED_03 = (1, 0, 1, 7, 0)
-    UNUSED_04 = (1, 0, 2, 7, 0)
+    POWER_DEL_ACTUAL_KW = (1, 0, 41, 7, 0)
+    POWER_RET_ACTUAL_KW = (1, 0, 42, 7, 0)
+
     UNUSED_05 = (0, 0, 96, 7, 9)
     UNUSED_06 = (0, 0, 96, 7, 21)
     UNUSED_07 = (1, 0, 99, 97, 0)
@@ -69,134 +76,132 @@ class Drm4(Enum):
     UNUSED_14 = (0, 1, 96, 1, 0)
 
 
-class Drm4ReaderUploader:
-    SerialConfig = dict(port='/dev/ttyUSB0', baudrate=115200, timeout=20)
-    InfluxDbConfig = dict(username='admin', password='admin', database='p1data')
+def parse_telegram(ser, last_timestamp_electr, last_timestamp_gas):
+    t_content = []
+    telegram_info = {}
 
-    InfLoopInterval = 0
+    awaiting_start = True
 
-    def __init__(self):
-        self.serial = serial.Serial(**self.SerialConfig)
-        self.influx = InfluxDBClient(**self.InfluxDbConfig)
+    while True:
+        line = ser.readline().decode('utf-8').strip()
 
-        self.last_dt_gas = None
-        self.last_dt_electricity = None
+        # Preparing CRC
+        t_content.append(line)
 
-        self._init_datetime_fields()
+        if not line:
+            continue
 
-    def _init_datetime_fields(self):
-        if res := list(self.influx.query('SELECT time, gas_time FROM p1 ORDER BY time DESC LIMIT 1').get_points('p1')):
-            self.last_dt_gas = TZ_INFLUX.localize(datetime.strptime(res[0]['gas_time'], INFLUX_DT_FMT))
+        if awaiting_start:
+            if line.startswith('/'):
+                awaiting_start = False
+            else:
+                logging.debug(f'Ignored line while waiting for start char: "{line}"')
+            continue
 
-        if res := list(
-                self.influx.query('SELECT time, power_delivered_w FROM p1 ORDER BY time DESC LIMIT 1').get_points(
-                    'p1')):
-            self.last_dt_electricity = TZ_INFLUX.localize(datetime.strptime(res[0]['time'], INFLUX_DT_FMT))
+        if line.startswith('!'):
+            t_content.append('!')
+            data = DRM4_LINE_SEP.join(t_content)
+            crc_calc = hex(drm4_crc(data.encode('utf-8')))
+            logging.info(f'End of telegram reached. Sending info ... {crc_calc}{line}')
+            break
 
-    def parse_telegram(self):
-        t_content = []
-        telegram_info = {}
+        if not (drm4_id := tel_id_re.search(line)):
+            logging.warning(f'Ignoring unknown DRM4 ID in: "{line}"')
+            continue
 
-        awaiting_start = True
+        try:
+            field = Drm4(tuple(int(x) for x in drm4_id.groups()))
 
-        while True:
-            line = self.serial.readline().decode('utf-8').strip()
+        except ValueError:
+            logging.warning(f'Ignoring non-implemented field {drm4_id[0]}. Line: "{line}".')
+            continue
 
-            # Preparing CRC
-            t_content.append(line)
+        if g_content := tel_content_re_extra.search(line):
+            converted = [float(x) for x in g_content.groups('nan') if x]
+            if len(converted) < 2:
+                converted = [float('nan'), converted[0]]  # The 1st match failed, we make it NaN
 
-            if not line:
-                continue
+            value, extra = converted
 
-            if awaiting_start:
-                if line.startswith('/'):
-                    awaiting_start = False
-                else:
-                    logging.debug(f'Ignored line while waiting for start char: "{line}"')
-                continue
+        else:
+            logging.warning(f'Read info field {field}. {line}.')
+            continue
 
-            if line.startswith('!'):
-                t_content.append('!')
-                data = DRM4_LINE_SEP.join(t_content)
-                crc_calc = hex(drm4_crc(data.encode('utf-8')))
-                logging.info(f'End of telegram reached. Sending info ... {crc_calc}{line}')
+        if field == Drm4.TIMESTAMP_ELECTR:
+            dt = parse_dt_to_utc(value)
+            if last_timestamp_electr and dt <= last_timestamp_electr:
+                logging.warning(f'Ignoring telegram. Timestamp is repeated /old: {dt} <= {last_timestamp_electr}')
+                telegram_info = None
                 break
 
-            if not (drm4_id := tel_id_re.search(line)):
-                logging.warning(f'Ignoring unknown DRM4 ID in: "{line}"')
+            telegram_info['dt_electricity'] = dt
+
+        elif field == Drm4.READ_DEL_T1_KWH:
+            telegram_info['energy_t1'] = float(value)
+        elif field == Drm4.READ_DEL_T2_KWH:
+            telegram_info['energy_t2'] = float(value)
+
+        elif field == Drm4.TARIFF_INDICATOR:
+            telegram_info['tariff_indicator'] = int(value)
+
+        elif field == Drm4.POWER_DEL_ACTUAL_KW:
+            telegram_info['power_delivered_w'] = 1000 * float(value)
+        elif field == Drm4.POWER_RET_ACTUAL_KW:
+            telegram_info['power_returned_w'] = 1000 * float(value)
+
+        elif field == Drm4.CURRENT_A:
+            telegram_info['current_delivered'] = float(value)
+
+        elif field == Drm4.GAS_T_VOLUME_M3:
+            dt = parse_dt_to_utc(value)
+
+            if last_timestamp_gas and dt <= last_timestamp_gas:
+                # Ignoring Gas info. Timestamp is repeated
                 continue
 
-            try:
-                field = Drm4(tuple(int(x) for x in drm4_id.groups()))
+            telegram_info['gas'] = float(extra)
+            telegram_info['gas_time'] = dt.strftime(INFLUX_DT_FMT)
+            telegram_info['gas_dt'] = dt
 
-            except ValueError:
-                logging.warning(f'Ignoring non-implemented field {drm4_id[0]}. Line: "{line}".')
-                continue
+    return telegram_info
 
-            if g_content := tel_content_re_extra.search(line):
-                converted = [float(x) for x in g_content.groups('nan') if x]
-                if len(converted) < 2:
-                    converted = [float('nan'), converted[0]]  # The 1st match failed, we make it NaN
 
-                value, extra = converted
+def main():
 
-            else:
-                logging.warning(f'Read info field {field}. {line}.')
-                continue
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=20)
+    local_client = influxdb.InfluxDBClient(username='admin', password='admin2', database='p1data')
 
-            match field:
+    token = os.environ.get("INFLUXDB_TOKEN")
 
-                case Drm4.TIMESTAMP_ELECTRICITY:
-                    dt = self._parse_dt_to_utc(value)
-                    if self.last_dt_electricity and dt <= self.last_dt_electricity:
-                        logging.warning(
-                            f'Ignoring telegram. Timestamp is repeated /old: {dt} <= {self.last_dt_electricity}')
-                        telegram_info = None
-                        break
 
-                    telegram_info['dt_electricity'] = dt
+    from influxdb_client import InfluxDBClient
 
-                case Drm4.READ_DEL_T1_KWH:
-                    telegram_info['energy_t1'] = float(value)
-                case Drm4.READ_DEL_T2_KWH:
-                    telegram_info['energy_t2'] = float(value)
+    influx_cloud_client = InfluxDBClient(
+            url = "https://eu-central-1-1.aws.cloud2.influxdata.com",
+            org = "franciscoruizvillar@gmail.com",
+            token = token
+    )
 
-                case Drm4.TARIFF_INDICATOR:
-                    telegram_info['tariff_indicator'] = int(value)
 
-                case Drm4.POWER_DEL_KW:
-                    telegram_info['power_delivered_w'] = 1000 * float(value)
+    last_dt_gas = None
+    if res := list(local_client.query('SELECT time, gas_time FROM p1 ORDER BY time DESC LIMIT 1').get_points('p1')):
+        last_dt_gas = TZ_INFLUX.localize(datetime.strptime(res[0]['gas_time'], INFLUX_DT_FMT))
 
-                case Drm4.CURRENT_A:
-                    telegram_info['current_delivered'] = float(value)
+    last_dt_electricity = None
+    if res := list(local_client.query('SELECT time, power_delivered_w FROM p1 ORDER BY time DESC LIMIT 1').get_points('p1')):
+        last_dt_electricity = TZ_INFLUX.localize(datetime.strptime(res[0]['time'], INFLUX_DT_FMT))
 
-                case Drm4.GAS_T_VOLUME_M3:
-                    dt = self._parse_dt_to_utc(value)
-
-                    if self.last_dt_gas and dt <= self.last_dt_gas:
-                        # Ignoring Gas info. Timestamp is repeated
-                        continue
-
-                    telegram_info['gas'] = float(extra)
-                    telegram_info['gas_time'] = dt.strftime(INFLUX_DT_FMT)
-
-        return telegram_info
-
-    def loop(self):
-        """ Runs `run` in a loop"""
-        while True:
-            self.run()
-            if self.InfLoopInterval:
-                time.sleep(self.InfLoopInterval)
-
-    def run(self):
-        fields = self.parse_telegram()
+    while True:
+        fields = parse_telegram(ser, last_dt_electricity, last_dt_gas)
 
         if not fields:
             raise RuntimeError('Unknown error: datagram could not be parsed')
 
-        if not (last_dt_electricity := fields.pop('dt_electricity', self.last_dt_electricity)):
+        last_dt_electricity = fields.pop('dt_electricity', last_dt_electricity)
+        if not last_dt_electricity:
             raise RuntimeError('Unknown error: datagram could not be parsed', fields)
+
+        last_dt_gas = fields.pop('gas_dt', last_dt_gas)
 
         tags = dict(tariff=fields.pop('tariff_indicator', None))
 
@@ -209,15 +214,26 @@ class Drm4ReaderUploader:
         }
 
         # Send the JSON data to InfluxDB
-        self.influx.write_points([data], time_precision='s')
-        logging.info(data)
 
-    @staticmethod
-    def _parse_dt_to_utc(dt_naive_f: str):
-        dt_naive = datetime.strptime(str(int(dt_naive_f)), DRM4_DT_FMT)
-        local = TZ_DRM4.localize(dt_naive)
-        return local.astimezone(pytz.utc)
+        try:
+            local_client.write_points([data], time_precision='s')
+            logging.info(data)
+        except InfluxDBServerError as e:
+            logging.error(f'Timeout error!{e}')
+
+
+        try:
+            influx_cloud_client.write_api().write('p1data', record=[data], time_precision='s')
+            logging.info(data)
+        except (AttributeError, InfluxDBServerError) as e:
+            logging.error(f'Timeout error!{e}')
+
+
+def parse_dt_to_utc(dt_naive_f: str):
+    dt_naive = datetime.strptime(str(int(dt_naive_f)), DRM4_DT_FMT)
+    local = TZ_DRM4.localize(dt_naive)
+    return local.astimezone(pytz.utc)
 
 
 if __name__ == '__main__':
-    Drm4ReaderUploader().loop()
+    main()
